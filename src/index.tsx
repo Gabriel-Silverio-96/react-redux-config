19cc24a935b1d9a6e19a367a00499f106ff2b2f3
Silvério <contato@gabrielsilver.com.br>
2021-07-21 19:16:48 -0300

    chore: added provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from 'Store/store';
+
+//Pages
 import Home from 'Pages/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Provider store={store}>
+      <Home />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
